cbb05585a835a9f98946e504b40a9a29f1b68a41
Tailine Chagas <tailinechagas@gmail.com>
2020-04-23 22:09:59 -0300

    feat: compression before the end

--- leetcode/string_compression.py
+++ leetcode/string_compression.py
@@ -1,20 +1,20 @@
-chars = ["a", "a", "b", "b", "c"]
+chars = ["a", "a", "a", "a", "b", "b", "b", "c", "c"]
 # def compress(self, chars: List[str]) -> int:
 curr_char = chars[0]
 count = 0
-# i = 0
+i = 0
 
-# while i < len(chars):
-for i, char in enumerate(chars):
+while i < len(chars) - 1:
     count += 1
-    if char == curr_char and count != 1:
+    if chars[i] == curr_char and count != 1:
         if chars[i + 1] == curr_char:
             chars.pop(i)
+            continue
         else:
             chars[i] = count
             count = 0
             curr_char = chars[i + 1]
-    # i += 1
+    i += 1
 # return chars
 print(chars)
 
