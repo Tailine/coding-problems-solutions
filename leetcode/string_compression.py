96c131fcb9d24b565fa3a5239899d7425bf2181b
Tailine Chagas <tailinechagas@gmail.com>
2020-04-25 01:05:00 -0300

    feat: correct compression at end

--- leetcode/string_compression.py
+++ leetcode/string_compression.py
@@ -1,17 +1,19 @@
-chars = ["a", "a", "a", "a", "b", "b", "b", "c", "c"]
+chars = ["a", "a", "b", "b"]
 # def compress(self, chars: List[str]) -> int:
 curr_char = chars[0]
 count = 0
 i = 0
 
-while i < len(chars) - 1:
+while i <= len(chars) - 1:
     count += 1
     if chars[i] == curr_char and count != 1:
-        if chars[i + 1] == curr_char:
+        if i == len(chars) - 1 and chars[i] == curr_char:
+            chars[i] = count
+        elif chars[i + 1] == curr_char:
             chars.pop(i)
             continue
         else:
-            chars[i] = count
+            chars[i] = str(count)
             count = 0
             curr_char = chars[i + 1]
     i += 1
