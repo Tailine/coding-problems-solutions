40be602757bc18728526acbeab7372e2b5e144fe
Tailine Chagas <tailinechagas@gmail.com>
2020-04-25 15:45:36 -0300

    feat: separate count into separated characters when it is greater than 9

--- leetcode/string_compression.py
+++ leetcode/string_compression.py
@@ -1,4 +1,4 @@
-chars = ["a", "b", "b", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c"]
+chars = ["a", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "b", "b"]
 # def compress(self, chars: List[str]) -> int:
 curr_char = chars[0]
 count = 0
@@ -8,7 +8,12 @@ while i <= len(chars) - 1:
     count += 1
     if chars[i] == curr_char and count != 1:
         if i == len(chars) - 1 and chars[i] == curr_char:
-            chars[i] = str(count)
+            str_count = str(count)
+            if count > 9:
+                chars.pop(i)
+                chars.extend(list(str_count))
+            else:
+                chars[i] = str_count
         elif chars[i + 1] == curr_char:
             chars.pop(i)
             continue
@@ -16,7 +21,7 @@ while i <= len(chars) - 1:
             chars[i] = str(count)
             count = 0
             curr_char = chars[i + 1]
-    elif chars[i] != curr_char:
+    elif chars[i] != curr_char and i < len(chars) - 2:
         curr_char = chars[i+1]
         count = 0
         continue
