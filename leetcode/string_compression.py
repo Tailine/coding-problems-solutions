1a1b18bffc47607d2485be6c360a9253f0ca509e
Tailine Chagas <tailinechagas@gmail.com>
2020-04-25 10:27:24 -0300

    feat: correct compression when first elem does not repeat

--- leetcode/string_compression.py
+++ leetcode/string_compression.py
@@ -1,4 +1,4 @@
-chars = ["a", "a", "b", "b"]
+chars = ["a", "b", "b", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c"]
 # def compress(self, chars: List[str]) -> int:
 curr_char = chars[0]
 count = 0
@@ -8,7 +8,7 @@ while i <= len(chars) - 1:
     count += 1
     if chars[i] == curr_char and count != 1:
         if i == len(chars) - 1 and chars[i] == curr_char:
-            chars[i] = count
+            chars[i] = str(count)
         elif chars[i + 1] == curr_char:
             chars.pop(i)
             continue
@@ -16,6 +16,10 @@ while i <= len(chars) - 1:
             chars[i] = str(count)
             count = 0
             curr_char = chars[i + 1]
+    elif chars[i] != curr_char:
+        curr_char = chars[i+1]
+        count = 0
+        continue
     i += 1
 # return chars
 print(chars)
