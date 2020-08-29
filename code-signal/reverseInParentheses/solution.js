a1607cbf40f7dfcb8e5bcf6467a98f9ab192ae0c
Tailine Chagas <tailinechagas@gmail.com>
2020-08-29 11:40:41 -0300

    feat: solve the reverseInParentheses challenge

--- code-signal/reverseInParentheses/solution.js
+++ code-signal/reverseInParentheses/solution.js
@@ -1,47 +1,21 @@
-function reverseString(splitedString) {
-  const stringCopy = [...splitedString.join("")];
-
-  return (start, finish) => {
-    const reversedWord = stringCopy.slice(start, finish).reverse();
-    stringCopy.splice(start, reversedWord.length, ...reversedWord);
-    return stringCopy;
-  };
-}
-
 function reverseInParentheses(inputString) {
   if (!inputString) {
     return inputString;
   }
 
-  const mappedParentheses = {};
   const openParenthesesPos = [];
+  let copyInputString = inputString;
 
-  const splitedString = inputString.split("");
-
-  splitedString.forEach((char, i) => {
-    if (char === "(") {
-      mappedParentheses[i] = null;
+  for (let i = 0; i < copyInputString.length; i++) {
+    if (copyInputString[i] === "(") {
       openParenthesesPos.unshift(i);
-    } else if (char === ")") {
-      const pos = openParenthesesPos.shift();
-      mappedParentheses[pos] = i;
+    } else if (copyInputString[i] === ")") {
+      const startPos = openParenthesesPos.shift();
+      const subStr = copyInputString.substring(startPos, i + 1);
+      const reversedStr = subStr.split("").reverse().join("");
+
+      copyInputString = copyInputString.replace(subStr, reversedStr);
     }
-  });
-
-  const reverseWord = reverseString(splitedString);
-
-  let reversedString;
-
-  const openArrPos = Object.keys(mappedParentheses);
-
-  for (let i = openArrPos.length; i > 0; i--) {
-    reversedString = reverseWord(
-      Number(openArrPos[i - 1]) + 1,
-      mappedParentheses[openArrPos[i - 1]]
-    );
   }
-
-  return reversedString.join("").replace(/[()]/g, "");
+  return copyInputString.replace(/[\(\)]/g, "");
 }
-
-console.log(reverseInParentheses("(abc)d(efg)(ba(acd))"));
