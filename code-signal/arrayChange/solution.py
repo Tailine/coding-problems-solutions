7ff456e14a0dcfdd244546ce4502af8ec61e0a10
Tailine Chagas <tailinechagas@gmail.com>
2020-08-31 13:23:44 -0300

    feat: solve arrayChange problem

---
+++ code-signal/arrayChange/solution.py
@@ -0,0 +1,11 @@
+def arrayChange(inputArray):
+    moves = 0
+    for i in range(0, len(inputArray)-1):
+        if inputArray[i] >= inputArray[i+1]:
+            increase = inputArray[i] - inputArray[i+1] + 1
+            inputArray[i+1] = increase + inputArray[i+1]
+            moves = moves + increase
+    print(moves)
+
+
+arrayChange([-1000, 0, -2, 0])
